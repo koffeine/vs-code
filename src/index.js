01129a14f690acb4ac325a305143c761fb9bfab0
Kornél Horváth <kornel.horvath@me.com>
2025-11-28 18:04:27 +0100

    Convert to templates/vs-code

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/** @param {import('vscode').ExtensionContext} context */
+// eslint-disable-next-line no-unused-vars
+export const activate = (context) => {
+	/* empty */
+};
+
+export const deactivate = () => { /* empty */ };
