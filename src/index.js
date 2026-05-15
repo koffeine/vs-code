20d8c1a5aae13ad697e18bb15da0b2289b08f691
Kornél Horváth <kornel.horvath@me.com>
2026-05-15 09:45:00 +0200

    Implement Open recent workspace

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import * as vscode from 'vscode';
 import { replaceInFolder } from './replace-in-folder.js';
+import { openRecentWorkspace } from './open-recent-workspace.js';
 
 /** @param {import('vscode').ExtensionContext} context */
 export const activate = (context) => {
 	context.subscriptions.push(
-		vscode.commands.registerCommand('koffeine.replaceInFolder', replaceInFolder)
+		vscode.commands.registerCommand('koffeine.replaceInFolder', replaceInFolder),
+		vscode.commands.registerCommand('koffeine.openRecentWorkspace', openRecentWorkspace)
 	);
 };
 
